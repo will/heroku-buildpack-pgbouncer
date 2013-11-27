20047e2d1b23576481fa87d80b0d91ed1a5dc721
Will Leinweber <will@bitfission.com>
2013-11-26 18:07:04 -0800

    would this bounce database_url and qc_database_url?

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -8,11 +8,21 @@ DB_HOST=${DB_URI[2]}
 DB_PORT=${DB_URI[3]}
 DB_NAME=${DB_URI[4]}
 
+QB=$(echo $QC_DATABASE_URL | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^postgres:\/\/([^:]+):([^@]+)@(.*?):(.*?)\/(.*?)(\\?.*)?$/')
+QB_URI=( $DB )
+QB_USER=${DB_URI[0]}
+QB_PASS=${DB_URI[1]}
+QB_HOST=${DB_URI[2]}
+QB_PORT=${DB_URI[3]}
+QB_NAME=${DB_URI[4]}
+
 if [ "$PGBOUNCER_PREPARED_STATEMENTS" == "false" ]
 then
   export PGBOUNCER_URI=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$DB_NAME?prepared_statements=false
+  export QC_PGBOUNCER_URI=postgres://$QB_USER:$QB_PASS@127.0.0.1:6000/$QB_NAME?prepared_statements=false
 else
   export PGBOUNCER_URI=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$DB_NAME
+  export QC_PGBOUNCER_URI=postgres://$QB_USER:$QB_PASS@127.0.0.1:6000/$QB_NAME
 fi
 
 
@@ -34,15 +44,24 @@ accept  = localhost:6002
 connect = $DB_HOST:$DB_PORT
 retry = yes
 
+[qc-heroku-postgres]
+client = yes
+protocol = pgsql
+accept  = localhost:6102
+connect = $QB_HOST:$QB_PORT
+retry = yes
+
 EOFEOF
 
 cat >> /app/vendor/pgbouncer/users.txt << EOFEOF
 "$DB_USER" "$DB_PASS"
+"$QB_USER" "$QB_PASS"
 EOFEOF
 
 cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
 [databases]
 $DB_NAME = host=localhost port=6002
+$QB_NAME = host=localhost port=6102
 [pgbouncer]
 listen_addr = localhost
 listen_port = 6000
